a652e43ca73eacda7e42e27afb0d91d75000b4df
Robert Kirberich <robert@kirberich.de>
2022-06-18 21:17:24 +0100

    Fix typing errors

--- gerber_to_scad/vector.py
+++ gerber_to_scad/vector.py
@@ -9,6 +9,7 @@ class V(object):
 
     def __unicode__(self):
         return "(%s, %s)" % (self.x, self.y)
+
     __repr__ = __unicode__
 
     @classmethod
@@ -21,17 +22,19 @@ class V(object):
 
     @classmethod
     def intersection(cls, o1, d1, o2, d2):
-        """ Find intersection of two vectors, if any """
+        """Find intersection of two vectors, if any"""
         try:
-            l2 = ((o2.x - o1.x) * d1.y / d1.x - o2.y + o1.y) / (d2.y - d2.x * d1.y / d1.x)
+            l2 = ((o2.x - o1.x) * d1.y / d1.x - o2.y + o1.y) / (
+                d2.y - d2.x * d1.y / d1.x
+            )
             return o2 + d2 * l2
         except ZeroDivisionError:
             return None
 
     @classmethod
     def point_line_projection(cls, v1, v2, p, limit_to_segment=False):
-        """ Returns the projection of the point p on the line defined
-            by the two endpoints v1 and v2
+        """Returns the projection of the point p on the line defined
+        by the two endpoints v1 and v2
         """
         d = v2 - v1
         l2 = d.abs_sq()
@@ -53,7 +56,7 @@ class V(object):
         return v1 + d * a
 
     def abs_sq(self):
-        """ Square of absolute value of vector self """
+        """Square of absolute value of vector self"""
         return abs(self.x * self.x + self.y * self.y)
 
     def consume_tuple(self, other):
@@ -62,18 +65,18 @@ class V(object):
         return other
 
     def cross(self, other):
-        """ cross product """
+        """cross product"""
         return V(self.x * other.y - other.x * self.y)
 
     def rotate(self, theta, as_degrees=False):
-        """ Adapted from https://gist.github.com/mcleonard/5351452.
-            Rotate this vector by theta in degrees.
+        """Adapted from https://gist.github.com/mcleonard/5351452.
+        Rotate this vector by theta in degrees.
         """
         if as_degrees:
             theta = math.radians(theta)
 
         dc, ds = math.cos(theta), math.sin(theta)
-        x, y = dc*self.x - ds*self.y, ds*self.x + dc*self.y
+        x, y = dc * self.x - ds * self.y, ds * self.x + dc * self.y
         return V(x, y)
 
     def __abs__(self):
@@ -83,7 +86,7 @@ class V(object):
         other = self.consume_tuple(other)
         if self.x == other.x and self.y == other.y:
             return 0
-        if self.abs() < other.abs():
+        if abs(self) < abs(other):
             return -1
         return 1
 
@@ -103,10 +106,10 @@ class V(object):
         other = self.consume_tuple(other)
         return V(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> "V":
         other = self.consume_tuple(other)
         if isinstance(other, V):
-            return (self.x * other.x + self.y * other.y)
+            return self.x * other.x + self.y * other.y
         return V(other * self.x, other * self.y)
 
     def __div__(self, other):
@@ -114,4 +117,5 @@ class V(object):
             raise Exception("Division by zero")
         other = float(other)
         return V(self.x / other, self.y / other)
+
     __truediv__ = __div__
