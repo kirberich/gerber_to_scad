9111dff52211c6cc346e9239f0952165d6743825
Rob Kirberich <robert@kirberich.de>
2026-06-02 10:28:26 +0100

    Make tests actually use test settings

--- tests/settings.py
+++ tests/settings.py
@@ -3,4 +3,4 @@ import os
 os.environ.setdefault("DEBUG", "false")
 os.environ.setdefault("SCAD_BINARY", "/usr/bin/openscad")
 
-from gts_service import settings  # pyright: ignore[reportUnusedImport]  # noqa: F401
+from gts_service.settings import *  # pyright: ignore[reportUnusedImport]  # noqa: F401, F403
